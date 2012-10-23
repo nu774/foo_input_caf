20f7dbe0b051a0d87b9ed2aa0dd3989baef5f3a6
nu774 <honeycomb77@gmail.com>
2012-10-23 22:18:05 +0900

    refactored

--- CAFMetaData.cpp
+++ CAFMetaData.cpp
@@ -2,31 +2,65 @@
 #include "strutil.h"
 #include "util.h"
 
+struct meta_handler_entry_t {
+    const char *key;
+    void (*handler)(file_info &, const char *);
+};
+
+namespace meta_to_fb2k {
+    void comments(file_info &pinfo, const char *value)
+    {
+	pinfo.meta_set("comment", value);
+    }
+    void encoding_application(file_info &pinfo, const char *value)
+    {
+	pinfo.info_set("tool", value);
+    }
+    void recorded_date(file_info &pinfo, const char *value)
+    {
+	pinfo.meta_set("record date", value);
+    }
+    void track_number(file_info &pinfo, const char *value)
+    {
+	std::vector<char> buf(std::strlen(value) + 1);
+	std::strcpy(&buf[0], value);
+	char *num, *total = &buf[0];
+	num = strutil::strsep(&total, "/");
+	pinfo.meta_set("tracknumber", num);
+	if (total && *total)
+	    pinfo.meta_set("totaltracks", total);
+    }
+    void year(file_info &pinfo, const char *value)
+    {
+	pinfo.meta_set("date", value);
+    }
+    const meta_handler_entry_t handlers[] = {
+	{ "comments",			comments		},
+	{ "encoding application",	encoding_application	},
+	{ "recorded date",		recorded_date		},
+	{ "track number",		track_number		},
+	{ "year",			year			},
+	{ 0,				0			}
+    };
+    void process_entry(file_info &pinfo, const char *key, const char *value)
+    {
+	const meta_handler_entry_t *entry = handlers;
+	for (; entry->key; ++entry) {
+	    if (!std::strcmp(key, entry->key)) {
+		entry->handler(pinfo, value);
+		return;
+	    }
+	}
+	pinfo.meta_set(key, value);
+    }
+}
+
 void CAFMetaData::getInfo(file_info &pinfo)
 {
     std::vector<kvpair_t>::const_iterator it;
-    for (it = m_meta.begin(); it != m_meta.end(); ++it) {
-	std::string key = it->first;
-	std::string value = it->second;
-	if (key == kAFInfoDictionary_Year)
-	    key = "date";
-	else if (key == kAFInfoDictionary_Comments)
-	    key = "comment";
-	else if (key == kAFInfoDictionary_EncodingApplication) {
-	    pinfo.info_set("tool", value.c_str());
-	    continue;
-	} else if (key == kAFInfoDictionary_TrackNumber) {
-	    std::vector<char> buf(value.size() + 1);
-	    std::strcpy(&buf[0], value.c_str());
-	    char *num, *total = &buf[0];
-	    num = strutil::strsep(&total, "/");
-	    pinfo.meta_set("tracknumber", num);
-	    if (total && *total)
-		pinfo.meta_set("totaltracks", total);
-	    continue;
-	}
-	pinfo.meta_set(key.c_str(), value.c_str());
-    }
+    for (it = m_meta.begin(); it != m_meta.end(); ++it)
+	meta_to_fb2k::process_entry(pinfo, it->first.c_str(),
+				    it->second.c_str());
 }
 
 uint64_t CAFMetaData::nextChunk(char *name)
