01d0e50a5d9a0e936b7fd48163dc17f52a573a82
nu774 <honeycomb77@gmail.com>
2013-01-21 21:50:38 +0900

    bump version

--- input_caf.h
+++ input_caf.h
@@ -2,7 +2,7 @@ DECLARE_FILE_TYPE("CAF files","*.CAF");
 
 DECLARE_COMPONENT_VERSION(
     "CAF Decoder",
-    "0.0.6",
+    "0.0.7",
     "CAF decoder based on CoreAudioToolbox.dll"
 );
 VALIDATE_COMPONENT_FILENAME("foo_input_caf.dll");
