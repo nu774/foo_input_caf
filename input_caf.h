830dffdf77e64780a4c4f41c9cb86bf3e5739f11
nu774 <honeycomb77@gmail.com>
2018-07-30 00:20:52 +0900

    bump

--- input_caf.h
+++ input_caf.h
@@ -2,7 +2,7 @@ DECLARE_FILE_TYPE("CAF files","*.CAF");
 
 DECLARE_COMPONENT_VERSION(
     "CAF Decoder",
-    "0.2.1",
+    "0.3.0",
     "CAF(Core Audio Format) input plugin, Copyright (C) 2014 nu774"
 );
 VALIDATE_COMPONENT_FILENAME("foo_input_caf.dll");
